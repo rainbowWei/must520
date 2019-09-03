d1ec5fa4608b5a15750690b3de7f5beb0e13339a
Rainbow <rainbow_weic@163.com>
2019-09-03 18:03:36 +0800

    首页

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,46 @@
 <template>
   <div class="container">
-    <div>
-      <h1 class="title">
-        must520
-      </h1>
-      <h2 class="subtitle">
-        must official website for xingxiwang.com
-      </h2>
-      <div class="t">
-        author: rainbow
-      </div>
-
-      <section>
-        <el-button type="primary">主要按钮</el-button>
-        <el-button type="success" plain>主要按钮</el-button>
-        <el-button type="danger" round>主要按钮</el-button>
-      </section>
-      <p>自定义主题按钮OK</p>
-
-      <p>图标OK</p>
-      <r-icon name="el-icon-arrow-right" size="16" />
-    </div>
-    <!-- <r-logo /> -->
+    <el-carousel class="carousel" :interval="2000" arrow="always" :autoplay="true">
+        <el-carousel-item class="xxxx" v-for="item in carousel" :key="item.src">
+          <!-- <img :src="item.src" alt=""> -->
+          <img src="@/assets/img/banner2.jpg" alt="">
+        </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
-
 <script>
-// import RLogo from '@/components/common/Logo'
 export default {
-  components: {
-    // RLogo,
-  },
   head: {
     title: '首页',
     meta: [
       { content: 'xxx', name: 'rainbow' }
     ]
+  },
+  data() {
+    return {
+      carousel: [
+        { src: "@/assets/img/banner.jpg"},
+        { src: "@/assets/img/banner2.jpg"},
+        { src: "@/assets/img/banner3.jpg" },
+        
+      ],
+    }
   }
 }
 </script>
-
 <style lang="scss" scoped>
-  .container {
-    color: #338;
-    .title {
-      font-size: 18px;
-      font-weight: bold;
+  .container .carousel {
+    .el-carousel__button{
+      background-color: blue !important;
+      width: 20px !important;
+      height: 20px !important;
+      border-radius: 50% !important;
     }
-    .subtitle {
-      color: #666;
-      font-weight: bold;
-      font-size: 12px;
-    }
-    .txt {
-      background: yellowgreen;
-    }
-    p {
-      margin-top: 16px;
+    img{
+      width:100%;
+      height: 100%;
     }
   }
+  
 </style>
 
